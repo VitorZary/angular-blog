061a8547204945f52777ee06ba04c5c40fc74bad
Vitor Hugo Kuhn Zary <vitorhbts@.com>
2023-05-13 22:54:07 -0300

    Propriedades dinâmicas dos componentes

--- src/app/components/title-blog/title-blog.component.ts
+++ src/app/components/title-blog/title-blog.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-title-blog',
   templateUrl: './title-blog.component.html',
-  styleUrls: ['./title-blog.component.css']
+  styleUrls: [
+    './title-blog.component.css',
+    './title-blog.responsive.component.css'
+  ]
 })
 export class TitleBlogComponent implements OnInit {
 
